e5e1a2befe9508c9c87f2479c7aa43e722d313f8
ajayg138 <ajaygole728@gmail.com>
2025-02-24 22:39:34 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String args[]){
+        int[] arr={1,3,6,9,12,15};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
