d198193593daa16f4b8a88620cf265ce119b6cbc
ajayg138 <ajaygole728@gmail.com>
2025-02-26 00:48:38 +0530

    Optimal Approach

--- printAllDivisor.java
+++ printAllDivisor.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class printAllDivisor {
 
     //Brute force approach...
-    public static void divisor(int num){
-        for(int i=1;i<=num;i++){
+//    public static void divisor(int num){
+//        for(int i=1;i<=num;i++){
+//            if(num%i==0){
+//                System.out.print(i+" ");
+//            }
+//        }
+//    }
+
+    //Optimal Approach
+    public static ArrayList<Integer> divisor(int num){
+        ArrayList<Integer> divisors=new ArrayList<>();
+
+        for(int i=1;i*i<=num;i++){
             if(num%i==0){
-                System.out.print(i+" ");
+                divisors.add(i);
+                if(num/i != i){
+                    divisors.add(num/i);
+                }
             }
         }
+        return divisors;
     }
 
     public static void main(String[] args){
@@ -16,6 +32,11 @@ public class printAllDivisor {
         System.out.print("Enter Num: ");
         int num=sc.nextInt();
 
-        divisor(num);
+//        divisor(num);
+
+        ArrayList<Integer> divisors = divisor(num);
+        for(int ans:divisors){
+            System.out.println(ans+" ");
+        }
     }
 }
