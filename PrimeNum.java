c8dbc02674e6934c15f1179398758a7e3bc1922c
ajayg138 <ajaygole728@gmail.com>
2025-02-26 01:20:32 +0530

    Origin master

--- PrimeNum.java
+++ PrimeNum.java
@@ -3,19 +3,40 @@ import java.util.Scanner;
 public class PrimeNum {
     //Brute Force Approach
 
+//    public static void checkPrime(int n){
+//        int cnt=0;
+//
+//        for(int i=1;i<=n;i++){
+//            if(n%i==0){
+//                cnt++;
+//            }
+//        }
+//        if(cnt==2){
+//            System.out.println("Prime Number it is...");
+//        }else{
+//            System.out.println("Not a Prime Number...");
+//        }
+//    }
+
+    //Optimal Approach
     public static void checkPrime(int n){
         int cnt=0;
 
-        for(int i=1;i<=n;i++){
+        for(int i=1; i*i<=n;i++){
             if(n%i==0){
                 cnt++;
+                if(i != n/i){
+                    cnt++;
+                }
             }
+
         }
         if(cnt==2){
             System.out.println("Prime Number it is...");
         }else{
             System.out.println("Not a Prime Number...");
         }
+
     }
 
     public static void main(String[] args){
