2be2cfc3e35e0c5c3ff69590d4263163654f6c44
ajayg138 <ajaygole728@gmail.com>
2025-02-27 00:01:11 +0530

    GCD Optimal Approach

--- GCD.java
+++ GCD.java
@@ -15,16 +15,34 @@ public class GCD {
 //        return gcd;
 
     //Better Approach
-    public static int greatestCommonDivisor(int n1, int n2){
-        int gcd=1;
+//    public static int greatestCommonDivisor(int n1, int n2){
+//        int gcd=1;
+//
+//        for(int i=Math.min(n1,n2);i>=1;i--){
+//            if(n1%i==0 && n2%i==0){
+//                gcd=i;
+//                break;
+//            }
+//        }
+//        return gcd;
+//    }
 
-        for(int i=Math.min(n1,n2);i>=1;i--){
-            if(n1%i==0 && n2%i==0){
-                gcd=i;
-                break;
+    //Optimal Approach using Euclidean Algorithm...
+
+    public static void greatestCommonDivisor(int n1, int n2){
+
+        while(n1>0 && n2>0){
+            if(n1>n2){
+                n1=n1%n2;
+            }else{
+                n2=n2%n1;
+            }
+            if(n1 != 0){
+                System.out.println("GCD is: "+n1);
+            }else{
+                System.out.println("GCD is: "+n2);
             }
         }
-        return gcd;
     }
 
     public static void main(String[] args){
@@ -35,8 +53,10 @@ public class GCD {
         System.out.print("Enter n2: ");
         int n2=sc.nextInt();
 
-        int ans = greatestCommonDivisor(n1,n2);
+//        int ans = greatestCommonDivisor(n1,n2);
+//
+//        System.out.println("GCD of "+n1 +" and "+n2+" is "+ans);
 
-        System.out.println("GCD of "+n1 +" and "+n2+" is "+ans);
+        greatestCommonDivisor(n1,n2);
     }
 }
