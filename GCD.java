59b909254a8a8160e831f1e11b4c3317b3e9ab5a
ajayg138 <ajaygole728@gmail.com>
2025-02-26 23:50:21 +0530

    GCD Better Approach

--- GCD.java
+++ GCD.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
 
 public class GCD {
-    public static int greatestCommonDivisor(int n1,int n2){
+
+    //Brute Force Approach...
+//    public static int greatestCommonDivisor(int n1,int n2){
+//        int gcd=1;
+//
+//        for(int i=1;i<=Math.min(n1,n2);i++){
+//            if(n1%i==0 && n2%i==0){
+//                gcd=i;
+//            }
+//        }
+//
+//        return gcd;
+
+    //Better Approach
+    public static int greatestCommonDivisor(int n1, int n2){
         int gcd=1;
 
-        for(int i=1;i<=Math.min(n1,n2);i++){
+        for(int i=Math.min(n1,n2);i>=1;i--){
             if(n1%i==0 && n2%i==0){
                 gcd=i;
+                break;
             }
         }
-
         return gcd;
     }
 
