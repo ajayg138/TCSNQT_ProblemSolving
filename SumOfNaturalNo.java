82b14b17994ba9ab606a17273d05a305c3ee0eb0
ajayg138 <ajaygole728@gmail.com>
2025-03-18 02:53:56 +0530

    Sum of Natural No.

--- SumOfNaturalNo.java
+++ SumOfNaturalNo.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
 
 public class SumOfNaturalNo {
-    public static int SumN(int n){
-        int ans = (n*(n+1))/2;
 
-        return ans;
+    //by using formula...
+//    public static int SumN(int n){
+//        int ans = (n*(n+1))/2;
+//
+//        return ans;
+//    }
+
+    //by using loop
+    public static int SumN(int n){
+        int sum=0;
+
+        for(int i=0;i<=n;i++){
+            sum += i;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
