3302d871a970bdc2bb11d5840bfacaa58f64e503
ajayg138 <ajaygole728@gmail.com>
2025-03-20 17:13:33 +0530

    GreaterElementBruteForce

---
+++ GreaterElement.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GreaterElement {
+    public static int countGreaterElement(int[] arr){
+        int n=arr.length;
+        int cnt = 1;
+
+        for(int i=1;i<n;i++){
+            boolean isGreater=true;
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[i]){
+                    isGreater=false;
+                    break;
+                }
+            }
+            if(isGreater){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int ans=countGreaterElement(arr);
+        System.out.print(ans);
+    }
+}
