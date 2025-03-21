1926060ca1483ffe1e6908ab8d3bd45d04e1ad2a
ajayg138 <ajaygole728@gmail.com>
2025-03-21 15:51:31 +0530

    Optimal Appraoch

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -2,14 +2,29 @@ import java.util.Scanner;
 
 public class PrimeOrNot {
 
+//    public static int checkPrime(int n){
+//        int divisor=0;
+//
+//        for(int i=1;i<n;i++){
+//            if(n%i==0){
+//                divisor++;
+//            }
+//        }
+//        return divisor;
+//    }
+
     public static int checkPrime(int n){
         int divisor=0;
 
-        for(int i=1;i<n;i++){
+        for(int i=1;i*i<=n;i++){
             if(n%i==0){
                 divisor++;
             }
+            if(n/i != i){
+                divisor++;
+            }
         }
+
         return divisor;
     }
 
