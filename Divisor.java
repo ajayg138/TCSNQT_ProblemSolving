032bd9b8ba1a29baf2a5d0869d3e0d6141ed5c74
ajayg138 <ajaygole728@gmail.com>
2025-03-21 15:25:05 +0530

    Divisor Optimal Solution

--- Divisor.java
+++ Divisor.java
@@ -1,15 +1,33 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Divisor {
     //Brute force Approach
 
-    public static void printDivisor(int n){
+//    public static void printDivisor(int n){
+//
+//        for(int i=1;i<=n;i++){
+//            if(n%i==0){
+//                System.out.println(i);
+//            }
+//        }
+//    }
 
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                System.out.println(i);
+    //Optimal Approach
+
+    public static ArrayList<Integer> printDivisor(int n){
+        ArrayList<Integer> divisors=new ArrayList<>();
+
+        for(int i=1;i*i<=n;i++){
+            if(n%i == 0){
+                divisors.add(i);
+                if(n/i != i){
+                    divisors.add(n/i);
+                }
             }
         }
+        return divisors;
     }
 
     public static void main(String[] args) {
@@ -17,6 +35,11 @@ public class Divisor {
         System.out.print("Enter n: ");
         int n=sc.nextInt();
 
-        printDivisor(n);
+//        printDivisor(n);
+        ArrayList<Integer> divisors=printDivisor(n);
+        for(int divisor:divisors){
+            System.out.println(divisor);
+        }
+
     }
 }
