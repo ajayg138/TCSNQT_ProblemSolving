1e51e69c2db484c82f73e3e1414162fd2fdea670
ajayg138 <ajaygole728@gmail.com>
2025-03-21 15:29:41 +0530

    PrimeNumber--BruteForceApproach

---
+++ PrimeOrNot.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeOrNot {
+
+    public static int checkPrime(int n){
+        int divisor=0;
+
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                divisor++;
+            }
+        }
+        return divisor;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+
+        int ans=checkPrime(n);
+        if(n==2){
+            System.out.print(n+" is prime no....");
+        }else{
+            System.out.print(n+" is not a prime no....");
+        }
+    }
+}
